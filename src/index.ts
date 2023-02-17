891b26bd7666c83993354b0b764fb1a31c51d46b
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-17 22:24:40 +0300

    added CORS and changeDir event

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,33 @@ import express from 'express'
 import {createServer} from 'http'
 import {Server} from 'socket.io'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import FileSystemService from './FileSystemService'
+import * as path from "path";
 
-dotenv.config({path: '../.env'})
+dotenv.config({path: path.join(__dirname, '..', '.env')})
 
 const app = express()
 const httpServer = createServer(app)
-const io = new Server(httpServer)
+const io = new Server(httpServer, {
+	cors: {
+		origin: '*',
+	}
+})
 
-io.on('connection', (socket) => console.log(`Client ${socket.id} connected`))
+const fileSystemService: FileSystemService = new FileSystemService()
+
+app.use(cors({
+	origin: '*'
+}))
+
+io.on('connection', (socket) => {
+	console.log(`Client ${socket.id} connected`)
+	socket.on('changeDir', newDir => {
+		fileSystemService.workingDirectory = newDir.trim()
+		console.log(fileSystemService.workingDirectory)
+	})
+})
 
 try {
 	const PORT = process.env.PORT || 5124
