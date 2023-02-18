6925321430510d5f3edbe257ee2ca78dc6001e63
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 22:49:27 +0300

    implemented previewFile method

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -1,12 +1,20 @@
 import {IFileController} from '../utils/interfaces/IFileController'
 import {Request, Response} from 'express'
 import FileService from '../services/FileService'
+import mime from "mime-types";
 
 class FileController implements IFileController {
 	downloadFile(req: Request, res: Response) {
-		const pathToFile: string = req.params.path
-		const absolutePath = FileService.downloadFile(pathToFile)
+		const pathToFile: string = req.query.path as string
+		const absolutePath = FileService.getAbsolutePathToFile(pathToFile)
 		res.download(absolutePath)
+	}
+
+	previewFile(req: Request, res: Response) {
+		const pathToFile: string = req.query.path as string
+		const absolutePath = FileService.getAbsolutePathToFile(pathToFile)
+		res.setHeader('Content-Type', `${mime.lookup(absolutePath) || 'text/plain'}; charset=utf-8`)
+		res.status(200).sendFile(absolutePath)
 	}
 }
 
