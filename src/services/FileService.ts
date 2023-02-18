5fd70d7a87d41c53d05a63c9ebf3f8eec98a8bad
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-18 22:48:45 +0300

    renamed downloadFile method to getAbsolutePathToFile

--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -2,7 +2,7 @@ import {IFileService} from '../utils/interfaces/IFileService'
 import * as path from 'path'
 
 class FileService implements IFileService {
-	downloadFile(pathToFile: string): string {
+	getAbsolutePathToFile(pathToFile: string): string {
 		return path.join(process.env.DISKSPACE_DIRECTORY as string, pathToFile)
 	}
 }
