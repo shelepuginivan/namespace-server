7f2292cde76e040a344360e0e4c2514e1d0c2d18
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-19 10:50:54 +0300

    renamed `getAbsolutePathToFile` method to `getAbsolutePathToItem`

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -7,13 +7,13 @@ import fileUpload from 'express-fileupload'
 class FileController implements IFileController {
 	downloadFile(req: Request, res: Response) {
 		const pathToFile: string = req.query.path as string
-		const absolutePath = FileService.getAbsolutePathToFile(pathToFile)
+		const absolutePath = FileService.getAbsolutePathToItem(pathToFile)
 		res.download(absolutePath)
 	}
 
 	previewFile(req: Request, res: Response) {
 		const pathToFile: string = req.query.path as string
-		const absolutePath = FileService.getAbsolutePathToFile(pathToFile)
+		const absolutePath = FileService.getAbsolutePathToItem(pathToFile)
 		res.setHeader('Content-Type', `${mime.lookup(absolutePath) || 'text/plain'}; charset=utf-8`)
 		res.status(200).sendFile(absolutePath)
 	}
