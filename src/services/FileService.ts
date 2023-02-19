41f6d58f3be589caffdab9aa091609983000c6d8
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-19 03:56:01 +0300

    implemented deleteItem method

--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -1,9 +1,18 @@
 import {IFileService} from '../utils/interfaces/IFileService'
 import * as path from 'path'
+import * as fs from 'fs/promises'
+import {resolveAbsoluteDiskSpacePath} from '../utils/resolveAbsoluteDiskSpacePath'
 
 class FileService implements IFileService {
 	getAbsolutePathToFile(pathToFile: string): string {
 		return path.join(process.env.DISKSPACE_DIRECTORY as string, pathToFile)
+	}
+
+	async deleteItem(itemPath: string): Promise<void> {
+		if (!path.isAbsolute(itemPath)) {
+			itemPath = resolveAbsoluteDiskSpacePath(itemPath)
+		}
+		await fs.rm(itemPath)
 	}
 }
 
