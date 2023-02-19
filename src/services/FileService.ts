95b1d4b77815ec5ba3d6818a9906f54c57784c9d
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-19 10:20:35 +0300

    implemented uploadFiles method

--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -1,7 +1,7 @@
 import {IFileService} from '../utils/interfaces/IFileService'
 import * as path from 'path'
 import * as fs from 'fs/promises'
-import {resolveAbsoluteDiskSpacePath} from '../utils/resolveAbsoluteDiskSpacePath'
+import fileUpload from 'express-fileupload'
 
 class FileService implements IFileService {
 	getAbsolutePathToFile(pathToFile: string): string {
@@ -10,9 +10,15 @@ class FileService implements IFileService {
 
 	async deleteItem(itemPath: string): Promise<void> {
 		if (!path.isAbsolute(itemPath)) {
-			itemPath = resolveAbsoluteDiskSpacePath(itemPath)
+			itemPath = this.getAbsolutePathToFile(itemPath)
 		}
 		await fs.rm(itemPath)
+	}
+
+	async uploadFiles(files: fileUpload.FileArray) {
+		Object.keys(files).forEach(pathToFile => {
+			(files[pathToFile] as fileUpload.UploadedFile).mv(this.getAbsolutePathToFile(pathToFile))
+		})
 	}
 }
 
