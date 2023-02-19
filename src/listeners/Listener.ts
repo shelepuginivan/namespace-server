4ac6cb2547eda3bea6dfdc9cbf3806968f089ab3
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-19 10:50:07 +0300

    refactored changeDir and deleteItem methods

--- src/listeners/Listener.ts
+++ src/listeners/Listener.ts
@@ -3,28 +3,23 @@ import {DisconnectReason, Server, Socket} from 'socket.io'
 import FileSystemItem from '../FileSystemItem'
 import FileSystemService from '../services/FileSystemService'
 import FileService from '../services/FileService'
+import * as path from 'path'
 
 class Listener implements IListener {
-	private fileSystemService: FileSystemService
-
-	constructor(fileSystemService: FileSystemService) {
-		this.fileSystemService = fileSystemService
-	}
-
 	disconnect(socket: Socket, reason: DisconnectReason): void {
 		console.log('[DISCONNECTED]'.red.bold, `${socket.id} - ${reason.toUpperCase()}`)
 	}
 
 	async changeDir(socket: Socket, newDirectory: string): Promise<void> {
-		this.fileSystemService.workingDirectory = newDirectory.trim()
-		const workingDirItems: FileSystemItem[] = await this.fileSystemService.getFilesInWorkingDirectory()
+		const workingDirItems: FileSystemItem[] = await FileSystemService.getFilesInDirectory(FileService.getAbsolutePathToItem(newDirectory))
 
 		socket.emit('updateDirItems', JSON.stringify(workingDirItems))
 	}
 
 	async deleteItem(io: Server, itemPath: string): Promise<void> {
 		await FileService.deleteItem(itemPath)
-		const updatedWorkingDirItems: FileSystemItem[] = await this.fileSystemService.getFilesInWorkingDirectory()
+		const absoluteItemPath = FileService.getAbsolutePathToItem(itemPath)
+		const updatedWorkingDirItems: FileSystemItem[] = await FileSystemService.getFilesInDirectory(path.dirname(absoluteItemPath))
 
 		io.sockets.emit('updateDirItems', JSON.stringify(updatedWorkingDirItems))
 	}
