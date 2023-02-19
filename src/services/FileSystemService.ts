63dc9abc8fa6a226dd050b85fc2ee613714dcb81
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-19 10:49:15 +0300

    removed private fields and refactored `getFilesInDirectory` method to be able to use the class without creating objects

--- src/services/FileSystemService.ts
+++ src/services/FileSystemService.ts
@@ -4,28 +4,13 @@ import * as fs from 'fs/promises'
 import FileSystemItem from '../FileSystemItem'
 
 class FileSystemService implements IFileSystemService {
-	private readonly root: string = process.env.DISKSPACE_DIRECTORY as string
-	private currentWorkingDirectory: string
-
-	constructor() {
-		this.currentWorkingDirectory = this.root
-	}
-
-	set workingDirectory(dir: string) {
-		this.currentWorkingDirectory = path.join(this.root, dir)
-	}
-
-	get workingDirectory(): string {
-		return this.currentWorkingDirectory
-	}
-
-	async getFilesInWorkingDirectory(): Promise<FileSystemItem[]> {
-		const workingDirectoryItems = await fs.readdir(this.workingDirectory)
+	async getFilesInDirectory(directory: string): Promise<FileSystemItem[]> {
+		const workingDirectoryItems = await fs.readdir(directory)
 		return workingDirectoryItems.map(item => {
-			const absolutePath = path.join(this.workingDirectory, item)
+			const absolutePath = path.join(directory, item)
 			return new FileSystemItem(absolutePath)
 		})
 	}
 }
 
-export default FileSystemService
+export default new FileSystemService()
