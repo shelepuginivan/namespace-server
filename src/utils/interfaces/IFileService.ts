2b32316ad6f6861806047733260966fcd122c6ca
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-21 20:54:05 +0300

    added implemented methods

--- src/utils/interfaces/IFileService.ts
+++ src/utils/interfaces/IFileService.ts
@@ -1,3 +1,8 @@
+import fileUpload from 'express-fileupload'
+
 export interface IFileService {
 	getAbsolutePathToItem(itemPath: string): string
+	getItemDirectory(itemPath: string): string
+	deleteItem(itemPath: string): Promise<void>
+	uploadFiles(files: fileUpload.FileArray): Promise<void>
 }
