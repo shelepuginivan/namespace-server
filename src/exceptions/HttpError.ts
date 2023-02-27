8fab391b1632e313622a1ac7565e35ca1412e8a8
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-28 01:32:45 +0300

    created extension of Error for http request errors

---
+++ src/exceptions/HttpError.ts
@@ -0,0 +1,11 @@
+class HttpError extends Error {
+	code: number
+
+	constructor(code: number, message: string) {
+		super()
+		this.code = code
+		this.message = message
+	}
+}
+
+export default HttpError
