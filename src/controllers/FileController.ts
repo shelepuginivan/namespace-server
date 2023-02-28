2ae0f5a4af64f08e8c1f78767274bf55ede6ab8a
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-28 07:06:53 +0300

    implemented error of sending error in middleware

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -1,26 +1,39 @@
 import {IFileController} from '../utils/interfaces/IFileController'
-import {Request, Response} from 'express'
+import {NextFunction, Request, Response} from 'express'
 import FileService from '../services/FileService'
 import mime from 'mime-types'
 import fileUpload from 'express-fileupload'
 
 class FileController implements IFileController {
-	downloadFile(req: Request, res: Response) {
-		const pathToFile: string = req.query.path as string
-		const absolutePath = FileService.getAbsolutePathToItem(pathToFile)
-		res.download(absolutePath)
+	downloadFile(req: Request, res: Response, next: NextFunction) {
+		try {
+			const pathToFile: string = req.query.path as string
+			const absolutePath = FileService.getAbsolutePathToItem(pathToFile)
+			res.download(absolutePath)
+		} catch (e) {
+			next(e)
+		}
 	}
 
-	previewFile(req: Request, res: Response) {
-		const pathToFile: string = req.query.path as string
-		const absolutePath = FileService.getAbsolutePathToItem(pathToFile)
-		res.setHeader('Content-Type', `${mime.lookup(absolutePath) || 'text/plain'}; charset=utf-8`)
-		res.status(200).sendFile(absolutePath)
+	previewFile(req: Request, res: Response, next: NextFunction) {
+		try {
+			const pathToFile: string = req.query.path as string
+			const absolutePath = FileService.getAbsolutePathToItem(pathToFile)
+			res.setHeader('Content-Type', `${mime.lookup(absolutePath) || 'text/plain'}; charset=utf-8`)
+			res.status(200).sendFile(absolutePath)
+		} catch (e) {
+			next(e)
+		}
 	}
 
-	async uploadFiles(req: Request, res: Response) {
-		await FileService.uploadFiles(req.files as fileUpload.FileArray)
-		res.status(200).end()
+	async uploadFiles(req: Request, res: Response, next: NextFunction) {
+		try {
+			await FileService.uploadFiles(req.files as fileUpload.FileArray)
+			res.status(200).end()
+		} catch (e) {
+			next(e)
+		}
+
 	}
 }
 
