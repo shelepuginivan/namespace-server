66d8df5d0e6a86c79e7fdf77933a4bef11d06a78
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-28 07:00:37 +0300

    added `next` argument to File controller methods' signature

--- src/utils/interfaces/IFileController.ts
+++ src/utils/interfaces/IFileController.ts
@@ -1,7 +1,7 @@
-import {Request, Response} from 'express'
+import {NextFunction, Request, Response} from 'express'
 
 export interface IFileController {
-	downloadFile(req: Request, res: Response): void
-	previewFile(req: Request, res: Response): void
-	uploadFiles(req: Request, res: Response): void
+	downloadFile(req: Request, res: Response, next: NextFunction): void
+	previewFile(req: Request, res: Response, next: NextFunction): void
+	uploadFiles(req: Request, res: Response, next: NextFunction): void
 }
