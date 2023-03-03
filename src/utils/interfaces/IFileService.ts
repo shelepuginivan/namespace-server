53a08ac0338fe5b4c1149c387cbb49e2b9892724
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-03 20:38:15 +0300

    added method `createDirectory`

--- src/utils/interfaces/IFileService.ts
+++ src/utils/interfaces/IFileService.ts
@@ -4,6 +4,7 @@ export interface IFileService {
 	getAbsolutePathToItem(itemPath: string): string
 	getItemDirectory(itemPath: string): string
 	deleteItem(itemPath: string): Promise<void>
+	createDirectory(newDirectory: string): Promise<void>
 	uploadFiles(files: fileUpload.FileArray): Promise<void>
 	renameItem(itemToRename: string, newName: string): Promise<void>
 }
