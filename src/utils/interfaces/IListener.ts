31b2370dea20b5665a2bf850eaf7d73028555491
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-03 20:34:14 +0300

    added `createDirectory` method

--- src/utils/interfaces/IListener.ts
+++ src/utils/interfaces/IListener.ts
@@ -3,6 +3,7 @@ import {DisconnectReason, Socket} from 'socket.io'
 export interface IListener {
 	connect(socket: Socket): void
 	changeDir(socket: Socket, newDirectory: string): Promise<void>
+	createDirectory(socket: Socket, newDirectory: string): Promise<void>
 	deleteItem(socket: Socket, itemPath: string): Promise<void>
 	disconnect(socket: Socket, reason: DisconnectReason): void
 	error(socket: Socket, error: Error): void
