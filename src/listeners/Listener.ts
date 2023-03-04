d94e603a6f8ec61eb4959783e3184e80cbc9934b
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-05 01:03:19 +0300

    implemented DI of `FileService` instance

--- src/listeners/Listener.ts
+++ src/listeners/Listener.ts
@@ -1,9 +1,15 @@
 import {IListener} from '../utils/interfaces/IListener'
 import {DisconnectReason, Socket} from 'socket.io'
 import FileSystemItem from '../FileSystemItem'
-import FileService from '../services/FileService'
+import {FileService} from '../services/FileService'
 
-class Listener implements IListener {
+export class Listener implements IListener {
+	private readonly _fileService: FileService
+
+	constructor(fileService: FileService) {
+		this._fileService = fileService
+	}
+
 	connect(socket: Socket): void {
 		try {
 			const password = socket.handshake.auth?.password
@@ -29,7 +35,7 @@ class Listener implements IListener {
 		try {
 			socket.rooms.forEach(room => socket.leave(room))
 			socket.join(newDirectory)
-			const workingDirItems: FileSystemItem[] = await FileService.getItemsInDirectory(FileService.getAbsolutePathToItem(newDirectory))
+			const workingDirItems: FileSystemItem[] = await this._fileService.getItemsInDirectory(this._fileService.getAbsolutePathToItem(newDirectory))
 
 			socket.emit('updateDirItems', JSON.stringify(workingDirItems))
 		} catch (e) {
@@ -39,13 +45,13 @@ class Listener implements IListener {
 
 	async createDirectory(socket: Socket, newDirectory: string): Promise<void> {
 		try {
-			const newDirectoryAbsolutePath = FileService.getAbsolutePathToItem(newDirectory)
-			const newDirectoryAbsoluteLocation = FileService.getItemDirectory(newDirectoryAbsolutePath)
-			const room = FileService.getItemDirectory(newDirectory)
+			const newDirectoryAbsolutePath = this._fileService.getAbsolutePathToItem(newDirectory)
+			const newDirectoryAbsoluteLocation = this._fileService.getItemDirectory(newDirectoryAbsolutePath)
+			const room = this._fileService.getItemDirectory(newDirectory)
 
-			await FileService.createDirectory(newDirectoryAbsolutePath)
+			await this._fileService.createDirectory(newDirectoryAbsolutePath)
 
-			const updatedFiles = JSON.stringify(await FileService.getItemsInDirectory(newDirectoryAbsoluteLocation))
+			const updatedFiles = JSON.stringify(await this._fileService.getItemsInDirectory(newDirectoryAbsoluteLocation))
 
 			socket.emit('updateDirItems', updatedFiles)
 			socket.to(room).emit('updateDirItems', updatedFiles)
@@ -56,10 +62,10 @@ class Listener implements IListener {
 
 	async deleteItem(socket: Socket, itemPath: string): Promise<void> {
 		try {
-			await FileService.deleteItem(itemPath)
+			await this._fileService.deleteItem(itemPath)
 
-			const relativeItemDirectory = FileService.getItemDirectory(itemPath)
-			const updatedDirectoryItems = JSON.stringify(await FileService.getItemsInDirectory(relativeItemDirectory))
+			const relativeItemDirectory = this._fileService.getItemDirectory(itemPath)
+			const updatedDirectoryItems = JSON.stringify(await this._fileService.getItemsInDirectory(relativeItemDirectory))
 
 			socket.emit('updateDirItems', updatedDirectoryItems)
 			socket.to(relativeItemDirectory).emit('updateDirItems', updatedDirectoryItems)
@@ -73,13 +79,13 @@ class Listener implements IListener {
 			if (oldPath.startsWith('/')) oldPath = oldPath.replace('/', '')
 			if (newPath.startsWith('/')) newPath = newPath.replace('/', '')
 
-			await FileService.renameItem(oldPath, newPath)
+			await this._fileService.renameItem(oldPath, newPath)
 
-			const oldRelativePath = FileService.getItemDirectory(oldPath)
-			const newRelativePath = FileService.getItemDirectory(newPath)
+			const oldRelativePath = this._fileService.getItemDirectory(oldPath)
+			const newRelativePath = this._fileService.getItemDirectory(newPath)
 
-			const itemsInOldDir = JSON.stringify(await FileService.getItemsInDirectory(oldRelativePath))
-			const itemsInNewDir = JSON.stringify(await FileService.getItemsInDirectory(newRelativePath))
+			const itemsInOldDir = JSON.stringify(await this._fileService.getItemsInDirectory(oldRelativePath))
+			const itemsInNewDir = JSON.stringify(await this._fileService.getItemsInDirectory(newRelativePath))
 
 			socket.emit('updateDirItems', itemsInOldDir)
 			socket.to(oldRelativePath).emit('updateDirItems', itemsInOldDir)
@@ -91,9 +97,9 @@ class Listener implements IListener {
 
 	async updateItems(socket: Socket, directory: string) {
 		try {
-			const absoluteItemPath = FileService.getAbsolutePathToItem(directory)
+			const absoluteItemPath = this._fileService.getAbsolutePathToItem(directory)
 
-			const updatedDirectoryItems = await FileService.getItemsInDirectory(absoluteItemPath)
+			const updatedDirectoryItems = await this._fileService.getItemsInDirectory(absoluteItemPath)
 
 			socket.emit('updateDirItems', JSON.stringify(updatedDirectoryItems))
 			socket.to(directory).emit('updateDirItems', JSON.stringify(updatedDirectoryItems))
@@ -106,5 +112,3 @@ class Listener implements IListener {
 		console.error('[ERROR]'.red.bold, `${socket.id} - ${error.message}`)
 	}
 }
-
-export default new Listener()
