a7a5a3a946426ad44fde76c1e7fd6c47e73f5d0f
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-05 10:54:46 +0300

    used arrow functions to prevent from `_fileService` injected in `FileController` to be undefined

--- src/routers/fileRouter.ts
+++ src/routers/fileRouter.ts
@@ -1,10 +1,13 @@
-import {Router} from 'express'
+import {NextFunction, Request, Response, Router} from 'express'
 import {fileController} from '../modules/fileModule'
 
 const fileRouter: Router = Router()
 
-fileRouter.get('/preview', fileController.previewFile)
-fileRouter.get('/', fileController.downloadFile)
-fileRouter.post('/', fileController.uploadFiles)
+fileRouter.get('/preview',
+	(req: Request, res: Response, next: NextFunction) => fileController.previewFile(req, res, next))
+fileRouter.get('/',
+	(req: Request, res: Response, next: NextFunction) => fileController.downloadFile(req, res, next))
+fileRouter.post('/',
+	(req: Request, res: Response, next: NextFunction) => fileController.uploadFiles(req,  res, next))
 
 export default fileRouter
