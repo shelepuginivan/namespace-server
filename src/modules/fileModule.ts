78e66e8987e34921d0108766acd1af0a5c7a65d1
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-07 00:28:23 +0300

    exported service instance instead of exporting controller and listener instances

--- src/modules/fileModule.ts
+++ src/modules/fileModule.ts
@@ -1,7 +1,3 @@
 import {FileService} from '../services/FileService'
-import {FileController} from '../controllers/FileController'
-import {Listener} from '../listeners/Listener'
 
-const fileService: FileService = new FileService()
-export const fileController: FileController = new FileController(fileService)
-export const listener: Listener = new Listener(fileService)
+export const fileService: FileService = new FileService()
