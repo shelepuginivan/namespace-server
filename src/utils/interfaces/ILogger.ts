509a9268a0b5b9f5dcbbbacaa4dfef622f0773d0
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-07 01:06:01 +0300

    added methods `connection` and `disconnection`

--- src/utils/interfaces/ILogger.ts
+++ src/utils/interfaces/ILogger.ts
@@ -1,4 +1,6 @@
 export interface ILogger {
+	connection(message: string): void
+	disconnection(message: string): void
 	error(e: Error | string): void
 	event(eventName: string, message: string): void
 	fatal(e: Error): void
