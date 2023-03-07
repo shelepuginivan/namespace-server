8cb948cafb3d828ac1c2b837b63610a49b927a69
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-07 20:45:58 +0300

    added type of `httpErrorMiddleware`

--- src/middlewares/httpErrorMiddleware.ts
+++ src/middlewares/httpErrorMiddleware.ts
@@ -1,9 +1,9 @@
-import {NextFunction, Request, Response} from 'express'
+import {ErrorRequestHandler, NextFunction, Request, Response} from 'express'
 
 import httpError from '../exceptions/HttpError'
 import {Logger} from '../utils/Logger'
 
-export const httpErrorMiddleware = (err: Error, _req: Request, res: Response, next: NextFunction) => {
+export const httpErrorMiddleware: ErrorRequestHandler = (err: Error, _req: Request, res: Response, next: NextFunction) => {
 	new Logger().error(err)
 
 	if (err instanceof httpError) {
