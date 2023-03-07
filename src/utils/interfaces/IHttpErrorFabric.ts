4d5515a84575e5a8a8a8f9d8ba5b6ae47b6744f5
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-07 13:50:41 +0300

    added method `createForbidden`

--- src/utils/interfaces/IHttpErrorFabric.ts
+++ src/utils/interfaces/IHttpErrorFabric.ts
@@ -2,5 +2,6 @@ import HttpError from '../../exceptions/HttpError'
 
 export interface IHttpErrorFabric {
 	createBadRequest(message: string): HttpError
+	createForbidden(message: string): HttpError
 	createInternalServerError(message: string): HttpError
 }
