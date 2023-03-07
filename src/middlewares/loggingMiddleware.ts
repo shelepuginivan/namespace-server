8d0e74e4a78b2b1801e4f61f931a569d6e9f651a
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-07 20:46:15 +0300

    created `loggingMiddleware`

---
+++ src/middlewares/loggingMiddleware.ts
@@ -0,0 +1,7 @@
+import {NextFunction, Request, RequestHandler, Response} from 'express'
+import {Logger} from '../utils/Logger'
+
+export const loggingMiddleware: RequestHandler = (req: Request, _res: Response, next: NextFunction): void => {
+	new Logger().request(req)
+	next()
+}
